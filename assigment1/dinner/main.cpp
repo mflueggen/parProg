b7c04040edb1c056990ea18dbe2cb2d9155e904d
mapp <mflueggen@student.hpi.de>
2019-05-06 17:03:29 -0400

    Fixes deadlock issue

--- assigment1/dinner/main.cpp
+++ assigment1/dinner/main.cpp
@@ -6,12 +6,13 @@
 
 struct philosopher {
 
-  philosopher(uint64_t dinners_eaten, pthread_mutex_t& lock_on_left_fork, pthread_mutex_t& lock_on_right_fork)
-    : dinners_eaten{dinners_eaten}, lock_on_left_fork{lock_on_left_fork}, lock_on_right_fork{lock_on_right_fork} {};
+  philosopher(uint64_t dinners_eaten, pthread_mutex_t& lock_on_left_fork, pthread_mutex_t& lock_on_right_fork, int id)
+    : dinners_eaten{dinners_eaten}, lock_on_left_fork{lock_on_left_fork}, lock_on_right_fork{lock_on_right_fork}, id{id} {};
 
   uint64_t dinners_eaten;
   pthread_mutex_t& lock_on_left_fork;
   pthread_mutex_t& lock_on_right_fork;
+  int id;
 };
 
 
@@ -22,14 +23,26 @@ void *philosopher_thread(void *args) {
 
   auto* phil = static_cast<philosopher*>(args);
 
+//  std::cout << "Starting " << phil->id << std::endl;
+
   while (_run) {
-    pthread_mutex_lock(&phil->lock_on_left_fork);
-    pthread_mutex_lock(&phil->lock_on_right_fork);
+    if (pthread_mutex_lock(&phil->lock_on_left_fork)) {
+      std::cout << "Phil " << phil->id << ": Could not lock left fork." << std::endl;
+    }
+    if (pthread_mutex_lock(&phil->lock_on_right_fork)) {
+      std::cout << "Phil " << phil->id << ": Could not lock right fork." << std::endl;
+    }
     ++phil->dinners_eaten;
-    pthread_mutex_unlock(&phil->lock_on_right_fork);
-    pthread_mutex_unlock(&phil->lock_on_left_fork);
+    if (pthread_mutex_unlock(&phil->lock_on_right_fork)) {
+      std::cout << "Phil " << phil->id << ": Could not unlock right fork." << std::endl;
+    }
+    if (pthread_mutex_unlock(&phil->lock_on_left_fork)) {
+      std::cout << "Phil " << phil->id << ": Could not unlock left fork." << std::endl;
+    }
   }
 
+
+//  std::cout << "Shutting down " << phil->id << std::endl;
   return nullptr;
 }
 
@@ -42,8 +55,8 @@ int main(int argc, char* argv[]) {
     throw  std::runtime_error("At least 2 philosophers are required.");
   }
 
-
   std::vector<philosopher> philosophers;
+  philosophers.reserve(philosopher_count);
   std::vector<pthread_t> threads(philosopher_count);
   std::vector<pthread_mutex_t> forks(philosopher_count);
 
@@ -53,14 +66,18 @@ int main(int argc, char* argv[]) {
 
   _run = true;
 
-  philosophers.emplace_back(0, forks[forks.size() - 1], forks[0]);
-  pthread_create(&threads[0], nullptr, philosopher_thread, &philosophers[0]);
+  philosophers.emplace_back(0, *(forks.end() - 1), forks.front(), 0);
+  if (pthread_create(&threads[0], nullptr, philosopher_thread, &philosophers[0])) {
+    std::cout << "pthread_create failed for philosopher 0." << std::endl;
+  }
 
   for (auto i = 1u; i < philosopher_count; ++i) {
     // left_lock
     // right_lock
-    philosophers.emplace_back(0, forks[i - 1], forks[i]);
-    pthread_create(&threads[i], nullptr, philosopher_thread, &philosophers[i]);
+    philosophers.emplace_back(0, forks[i], forks[i - 1], (int)i);
+    if (pthread_create(&threads[i], nullptr, philosopher_thread, &philosophers[i])) {
+      std::cout << "pthread_create failed for philosopher " << i << "." << std::endl;
+    }
   }
 
   sleep(runtime_in_sec);
