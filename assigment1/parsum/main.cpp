6bb7f28052f5dd9e7b662761ad33c96a53c8fca7
mapp <mflueggen@student.hpi.de>
2019-05-07 20:12:01 -0400

    Makes parsum great again

--- assigment1/parsum/main.cpp
+++ assigment1/parsum/main.cpp
@@ -10,37 +10,36 @@
 #include <fstream>
 #endif
 
-#include "InfInt.h"
 #include <iostream>
+#include <algorithm>
 #include <string>     // std::string, std::stoull
 #include <pthread.h>
+#include <vector>
 
-InfInt *junk_size;
-InfInt *thread_count;
-InfInt *start;
-InfInt *end;
+struct range {
+  range(unsigned __int128 from, unsigned __int128 to, unsigned __int128 sum)
+    : from{from}, to{to}, sum{sum} {}
+  unsigned __int128 from;
+  unsigned __int128 to;
+  unsigned __int128 sum;
+};
 
-void *slow_sum(void *index) {
+void* slow_sum(void* args) {
 #ifdef LOG
     std::ofstream logfile;
     logfile.open ((*((InfInt *)index)).toString() + ".txt");
     logfile << "Thread " << *((InfInt *)index) << "\n";
 #endif
-    InfInt result = 0;
-    InfInt upper_bound = (*start) + *((InfInt *)index)*(*junk_size) + (*junk_size) - 1;
-    if (upper_bound > (*end)) {
-        upper_bound = (*end);
-    }
-    for (InfInt i = (*start) + *((InfInt *)index)*(*junk_size); i <= upper_bound; ++i) {
-        result += i;
+    auto* r = static_cast<range*>(args);
+    r->sum = 0;
+    for (unsigned __int128 i = r->from; i < r->to; ++i) {
+        r->sum += i;
     }
 #ifdef LOG
     logfile << (*start) + *((InfInt *)index)*(*junk_size) << "-" << upper_bound << "\n";
     logfile << "Result: " << result << "\n";
     logfile.close();
 #endif
-    *((InfInt *)index) = result;
-    pthread_exit(nullptr);
     return nullptr;
 }
 
@@ -48,47 +47,68 @@ int main(int argc, char *argv[]) {
 #ifdef TIMER
     clock_t begin = clock();
 #endif
-    InfInt local_thread_count = (InfInt) std::stoull(argv[1]);
-    thread_count = &local_thread_count;
-    InfInt local_start = (InfInt) std::stoull(argv[2]);
-    start = &local_start;
-    InfInt local_end = (InfInt) std::stoull(argv[3]);
-    end = &local_end;
+  const auto thread_count = std::stoull(argv[1]);
+  const auto start = std::stoull(argv[2]);
+  const auto end = std::stoull(argv[3]);
+
 
 #ifdef DEV
     std::cout << "Threadcount: " << thread_count << std::endl;
     std::cout << "Calculating sum from " << start << " to " << end  << std::endl;
 #endif
 
-    InfInt local_junk_size = (((*end) - (*start)) / (*thread_count).toInt()) + 1;  // +1 to distribute the remainder to all threads
-    junk_size = &local_junk_size;
+    const unsigned __int128 chunk_size = (end - start + thread_count - 1) / thread_count;
 
     // Start each thread with a specific range to summate
-    pthread_t threads[(*thread_count).toInt()];
-    InfInt thread_com[(*thread_count).toInt()];
-    for (uint64_t j = 0; j < (*thread_count).toInt(); ++j) {
-        thread_com[j] = j;
-        int return_code = pthread_create(&threads[j], nullptr, slow_sum, (void *)&thread_com[j]);
-        if (return_code != 0) {
+    std::vector<pthread_t> threads(thread_count);
+    std::vector<range> ranges;
+    ranges.reserve(thread_count);
+
+
+    unsigned __int128 start_of_range = start;
+    unsigned __int128 end_of_range = 0;
+    for (auto i = 0ul; i < thread_count; ++i) {
+      end_of_range = start_of_range + chunk_size;
+      if (end_of_range >= end) {
+        end_of_range = end + 1;
+      }
+      ranges.emplace_back(start_of_range, end_of_range, 0);
+      start_of_range = end_of_range;
+      int return_code = pthread_create(&threads[i], nullptr, slow_sum, &ranges[i]);
 #ifdef DEV
-            std::cout << "Error while creating thread. Code: " << return_code << std::endl;
+      std::cout << "Error while creating thread. Code: " << return_code << std::endl;
 #endif
-            exit(-1);
-        }
     }
+
+
 
     // We assume that the total amount of threads is not big enough for further parallelism. (overhead > summation)
-    InfInt result = 0;
-    for (int i = 0; i < (*thread_count).toInt(); ++i) {
-        void *res;
-        pthread_join(threads[i], &res);
-        result += thread_com[i];
-#ifdef DEV
-        std::cout << "Teilergebnis " << i <<": " << (InfInt )results[i] << std::endl;
-#endif
+    unsigned __int128 sum = 0;
+    for (auto i = 0ul; i < thread_count; ++i) {
+      pthread_join(threads[i], nullptr);
+      sum += ranges[i].sum;
     }
 
-    std::cout << result << std::endl;
+    std::vector<int> sum_as_string;
+
+    while (sum > 0) {
+      int last_char = sum % 10;
+      sum /= 10;
+      sum_as_string.push_back(last_char);
+    }
+
+    if (sum_as_string.empty())
+      std::cout << 0;
+    else
+    {
+      for (auto it = sum_as_string.rbegin(); it != sum_as_string.rend(); ++it) {
+        std::cout << *it;
+      }
+    }
+
+  std::cout << std::endl;
+
+
 
 #ifdef TIMER
     clock_t finish = clock();
@@ -96,5 +116,5 @@ int main(int argc, char *argv[]) {
     std::cout << "Took " << elapsed_secs << " seconds" << std::endl;
 #endif
 
-    pthread_exit(nullptr);
+  return 0;
 }
