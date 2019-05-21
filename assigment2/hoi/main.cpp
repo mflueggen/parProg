8fe95aca51ca36582c2220220f518e02bd00db04
mapp <mflueggen@student.hpi.de>
2019-05-21 16:26:59 -0400

    Computes N md5s using OpenMP. Working on Optimization.

--- assigment2/hoi/main.cpp
+++ assigment2/hoi/main.cpp
@@ -1,34 +1,78 @@
 #include "openssl/md5.h"
 
 #include <algorithm>
+#include <array>
+#include <chrono>
+#include <cstring>
+#include <iomanip>
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <stdexcept>
+#include <vector>
+
+
+struct uint512 {
+  static const size_t SIZE = 512 / 8;
+
+  unsigned char data[SIZE];
+
+  inline void inc() {
+    for (auto b = SIZE - 1;; --b) {
+      ++data[b];
+      if(data[b] != 0) break;
+      if (b == 0) break;
+    }
+  }
+};
+
 
 int main(int count, char *args[]) {
 
-  const size_t BLOCK_SIZE = 512 / 8;
 
   if (count <= 3)
     throw std::runtime_error("wrong number of arguments");
 
 
-  unsigned char initial_block[BLOCK_SIZE];
-  std::fill(initial_block, initial_block + BLOCK_SIZE, 0);
+  uint512 data;
+  std::fill(data.data, data.data + uint512::SIZE, 0);
 
   for (auto i = 0; args[1][i] != '\0'; i+=2) {
-    std::sscanf(&args[1][i], "%2hhx", &initial_block[i/2]);
+    std::sscanf(&args[1][i], "%2hhx", &data.data[i/2]);
   }
 
-  unsigned char md[16];
+  const uint64_t N = std::atol(args[2]);
+  auto start = std::chrono::high_resolution_clock::now();
+  // idee berechne alle blöcke beforehand
+  std::vector<uint512> precomputed_data;
+  precomputed_data.resize(N);
 
-  MD5(initial_block, BLOCK_SIZE, md);
-
-
-  for (auto i = 0; i < 16; ++i) {
-    std::cout << std::hex << static_cast<int>(md[i]);
+  for (auto i = 0ul; i < N; ++i) {
+    std::memcpy(precomputed_data[i].data, data.data, uint512::SIZE);
+    data.inc();
   }
-  std::cout << std::endl;
+
+  auto end = std::chrono::high_resolution_clock::now();
+  std::cout << "Precompute data: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()  << " ms" << std::endl;
+
+
+
+  start = std::chrono::high_resolution_clock::now();
+  std::vector<std::array<unsigned char, 16>> md5s;
+  md5s.resize(N);
+#pragma omp parallel for default(none) shared(precomputed_data, md5s)
+  for (auto i = 0ul; i < N; ++i) {
+    MD5((unsigned char*)(precomputed_data[i].data), uint512::SIZE, md5s[i].data());
+  }
+  end = std::chrono::high_resolution_clock::now();
+  std::cout << "Compute md5s: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()  << " ms" << std::endl;
+
+//  for (auto j = 0; j < N; ++j) {
+//    for (auto i = 0; i < 16; ++i) {
+//      std::cout << std::hex << std::setfill('0') << std::setw(2) << static_cast<int>(md5s[j][i]);
+//    }
+//    std::cout << std::endl;
+//  }
 
 
 
