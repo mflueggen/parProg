89bd25acd5960a60b65a6adfe7e59d53d26d08f2
mapp <mflueggen@student.hpi.de>
2019-05-30 09:04:32 -0400

    Uses std::vector for heatmaps. Reduces branching overhead

--- assigment2/heatmap/main.cpp
+++ assigment2/heatmap/main.cpp
@@ -19,15 +19,17 @@ int main(int argc, char *argv[]) {
   if (argc < 5) throw std::runtime_error("Wrong number of arguments");
 
   // Initialize values
-  const auto width = std::stoi(argv[1]);
-  const auto height = std::stoi(argv[2]);
+  const auto width = std::stoi(argv[1]) + 2;
+  const auto height = std::stoi(argv[2]) + 2;
   const auto rounds = std::stoi(argv[3]);
+
+  using T = float;
 
   // we manage two heatmaps. Each round the "active" heatmap is updated based on the values of the other "old" heatmap
   // that way we don't need to synchronize data bounderies between threads.
   // a heatmap is modeled as an one dimensional array. Using _width and _height we can compute either the two
   // dimensional coordinate from the given index or compute the index given the coordinate.
-  std::vector<double> heatmaps[2];
+  std::vector<std::vector<T>> heatmaps[2];
 
   // vector of all hotspots
   std::vector<hotspot> hotspots = load_hotspots(argv[4]);
@@ -38,16 +40,21 @@ int main(int argc, char *argv[]) {
     coords = load_coords(argv[5]);
 
   // Initialize _heatmaps
-  heatmaps[0].resize(width * height);
-  heatmaps[1].resize(width * height);
+  heatmaps[0].resize(height);
+  for(auto& v : heatmaps[0])
+    v.resize(width);
+
+  heatmaps[1].resize(height);
+  for(auto& v : heatmaps[1])
+    v.resize(width);
 
 
-  const auto start = std::chrono::high_resolution_clock::now();
+//  const auto start = std::chrono::high_resolution_clock::now();
 
   for (const auto& h : hotspots) {
     if (h.start_round == 0)
       // activate hotspot on "old" heatmap
-      heatmaps[0][coordinate::coord_to_index({h.x, h.y}, width, height)] = 1.0;
+      heatmaps[0][h.y + 1][h.x + 1] = 1.0;
   }
 
 
@@ -59,37 +66,34 @@ int main(int argc, char *argv[]) {
 #ifdef WITH_OMP
 #pragma omp parallel for default(none) shared(heatmaps)
 #endif
-    for (auto i = 0ul; i < width * height; ++i) {
-      const auto coord = coordinate::index_to_coord(i, width);
-      double sum = 0;
-      // for the 3x3 matrix around coord
-      for (auto y = static_cast<int64_t>(coord.y) - 1; y <= static_cast<int64_t>(coord.y) + 1; ++y) {
-        for (auto x = static_cast<int64_t>(coord.x) - 1; x <= static_cast<int64_t>(coord.x) + 1; ++x) {
-          const auto index = coordinate::coord_to_index({static_cast<uint32_t>(x), static_cast<uint32_t>(y)}, width,
-                                                        height);
-          if (index < std::numeric_limits<uint32_t>::max()) {
-            sum += heatmaps[old_heatmap_index][index];
+    for (auto row = 1; row < height - 1; ++row) {
+      for (auto col = 1; col < width - 1; ++col) {
+        double sum = 0;
+        // for the 3x3 matrix around coord
+        for (auto y = row -1; y <= row + 1; ++y) {
+          for (auto x = col - 1; x <= col + 1; ++x) {
+            sum += heatmaps[old_heatmap_index][y][x];
           }
         }
+        heatmaps[current_heatmap_index][row][col] = sum / 9.0;
       }
-      heatmaps[current_heatmap_index][i] = sum / 9.0;
     }
 
     // set active hotspots back to one
     for (auto h = 0; h < hotspots.size(); ++h) {
       const auto& hotspot = hotspots[h];
       if (round >= hotspot.start_round && round < hotspot.end_round)
-        heatmaps[current_heatmap_index][coordinate::coord_to_index({hotspot.x, hotspot.y}, width, height)] = 1.0;
+        heatmaps[current_heatmap_index][hotspot.y + 1][hotspot.x + 1] = 1.0;
     }
 
     // wait until all threads are done working so we can safely switch the active and old heatmap.
     // ---> barrier here
   }
 
-  const auto end = std::chrono::high_resolution_clock::now();
-
-  std::cout << "Runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms"
-            << std::endl;
+//  const auto end = std::chrono::high_resolution_clock::now();
+//
+//  std::cout << "Runtime: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms"
+//            << std::endl;
 
   std::ofstream output_file("output.txt");
   output_file << std::setprecision(4) << std::fixed;
@@ -97,22 +101,20 @@ int main(int argc, char *argv[]) {
   // output heatmap
   if (coords.empty()) {
     // print result
-    auto index = 0u;
-    for (uint32_t y = 0; y < height; ++y) {
-      for (uint32_t x = 0; x < width; ++x) {
-        const auto value = heatmaps[rounds % 2][index];
+    for (uint32_t y = 1; y < height - 1; ++y) {
+      for (uint32_t x = 1; x < width - 1; ++x) {
+        const auto value = heatmaps[rounds % 2][y][x];
         if (value > 0.9) {
           output_file << 'X';
         } else {
           output_file << static_cast<uint32_t>((value + 0.09) * 10.0) % 10;
         }
-        ++index;
       }
       output_file << '\n';
     }
   } else {
     for (const auto& coord : coords) {
-      output_file << heatmaps[rounds % 2][coordinate::coord_to_index(coord, width, height)] << '\n';
+      output_file << heatmaps[rounds % 2][coord.y + 1][coord.x + 1] << '\n';
     }
   }
 
