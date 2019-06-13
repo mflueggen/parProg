aa7ac668668b1d5cd008185d7f553c5c92e20dab
Benedikt Bock <mail@benedikt1992.de>
2019-06-13 12:33:13 +0200

    start OpenCL Tutorial

---
+++ assignment3/anteru/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+
+#ifdef __APPLE__
+#include "OpenCL/opencl.h"
+#else
+#include "CL/cl.h"
+#endif
+
+std::string GetPlatformName (cl_platform_id id)
+{
+    size_t size = 0;
+    clGetPlatformInfo (id, CL_PLATFORM_NAME, 0, nullptr, &size);
+
+    std::string result;
+    result.resize (size);
+    clGetPlatformInfo (id, CL_PLATFORM_NAME, size,
+                       const_cast<char*> (result.data ()), nullptr);
+
+    return result;
+}
+
+std::string GetDeviceName (cl_device_id id)
+{
+    size_t size = 0;
+    clGetDeviceInfo (id, CL_DEVICE_NAME, 0, nullptr, &size);
+
+    std::string result;
+    result.resize (size);
+    clGetDeviceInfo (id, CL_DEVICE_NAME, size,
+                     const_cast<char*> (result.data ()), nullptr);
+
+    return result;
+}
+
+void CheckError (cl_int error, std::string msg)
+{
+    if (error != CL_SUCCESS) {
+        std::cerr << "OpenCL call failed with error " << error << ": " << msg << std::endl;
+        std::exit (1);
+    }
+}
+
+int main ()
+{
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetPlatformIDs.html
+    cl_uint platformIdCount = 0;
+    clGetPlatformIDs (0, nullptr, &platformIdCount);
+
+    if (platformIdCount == 0) {
+        std::cerr << "No OpenCL platform found" << std::endl;
+        return 1;
+    } else {
+        std::cout << "Found " << platformIdCount << " platform(s)" << std::endl;
+    }
+
+    std::vector<cl_platform_id> platformIds (platformIdCount);
+    clGetPlatformIDs (platformIdCount, platformIds.data (), nullptr);
+
+    for (cl_uint i = 0; i < platformIdCount; ++i) {
+        std::cout << "\t (" << (i+1) << ") : " << GetPlatformName (platformIds [i]) << std::endl;
+    }
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clGetDeviceIDs.html
+    cl_uint deviceIdCount = 0;
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_GPU, 0, nullptr,
+                    &deviceIdCount);
+
+    if (deviceIdCount == 0) {
+        std::cerr << "No OpenCL devices found" << std::endl;
+        return 1;
+    } else {
+        std::cout << "Found " << deviceIdCount << " device(s)" << std::endl;
+    }
+
+    std::vector<cl_device_id> deviceIds (deviceIdCount);
+    clGetDeviceIDs (platformIds [0], CL_DEVICE_TYPE_GPU, deviceIdCount,
+                    deviceIds.data (), nullptr);
+
+    for (cl_uint i = 0; i < deviceIdCount; ++i) {
+        std::cout << "\t (" << (i+1) << ") : " << GetDeviceName (deviceIds [i]) << std::endl;
+    }
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateContext.html
+    const cl_context_properties contextProperties [] =
+            {
+                    CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties> (platformIds [0]),
+                    0 // 0-terminated list
+            };
+
+    cl_int error = CL_SUCCESS;
+    cl_context context = clCreateContext (contextProperties, deviceIdCount,
+                                          deviceIds.data (), nullptr, nullptr, &error);
+    CheckError(error, "Context creation failed");
+
+    std::cout << "Context created" << std::endl;
+
+    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateCommandQueue.html
+    cl_command_queue queue = clCreateCommandQueue (context, deviceIds [0],
+                                                   0, &error);
+    CheckError(error, "Queue creation failed");
+
+    // Here were ready to actually run the code
+//    next part: https://anteru.net/blog/2012/getting-started-with-opencl-part-2/
+
+    clReleaseCommandQueue (queue);
+
+    clReleaseContext (context);
+}
