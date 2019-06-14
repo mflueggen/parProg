cf10c1a35ca5806dbe0205ed464bf9067d5aadff
Benedikt Bock <mail@benedikt1992.de>
2019-06-14 14:56:08 +0200

    use c++ bindings

--- assignment3/anteru/main.cpp
+++ assignment3/anteru/main.cpp
@@ -12,20 +12,7 @@
 #include "CL/cl2.hpp"
 #endif
 
-//std::string GetDeviceName (cl_device_id id)
-//{
-//    size_t size = 0;
-//    clGetDeviceInfo (id, CL_DEVICE_NAME, 0, nullptr, &size);
-//
-//    std::string result;
-//    result.resize (size);
-//    clGetDeviceInfo (id, CL_DEVICE_NAME, size,
-//                     const_cast<char*> (result.data ()), nullptr);
-//
-//    return result;
-//}
-
-void CheckError (cl_int error, std::string msg)
+void CheckError (cl_int error, const std::string& msg)
 {
     if (error != CL_SUCCESS) {
         std::cerr << "OpenCL call failed with error " << error << ": " << msg << std::endl;
@@ -33,38 +20,40 @@ void CheckError (cl_int error, std::string msg)
     }
 }
 
-//std::string LoadKernel (const char* name)
-//{
-//    std::ifstream in(name);
-//    std::string result (
-//            (std::istreambuf_iterator<char> (in)),
-//            std::istreambuf_iterator<char> ());
-//    return result;
-//}
-//
-//cl_program BuildProgram (const std::string& source,
-//                          cl_context context)
-//{
-//
-//    cl_int error = 0;
-//    cl::Program program(context, source.c_str());
-//    CheckError (error, "Create Program");
-//
-//    error = program.build();
-//    if (error == CL_BUILD_PROGRAM_FAILURE) {
-//        cl::vector<cl::Device> devices;
-//        devices = context.getInfo<CL_CONTEXT_DEVICES>();
-//        CheckError(devices.size() > 0 ? CL_SUCCESS : -1, devices.size() > 0);
-//        std::string log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0], NULL);
-//        std::cerr << "Build Log: " << log << std::endl;
-//    }
-//
-//
-//    return program;
-//}
+std::string LoadKernel (const char* name)
+{
+    std::ifstream in(name);
+    std::string result (
+            (std::istreambuf_iterator<char> (in)),
+            std::istreambuf_iterator<char> ());
+    return result;
+}
+
+cl::Program BuildProgram (const std::string& source,
+                          const cl::Context& context)
+{
+
+    cl_int error = 0;
+    cl::Program program(context, source);
+    CheckError (error, "Create Program");
+
+    error = program.build();
+    if (error == CL_BUILD_PROGRAM_FAILURE) {
+        cl::vector<cl::Device> devices;
+        devices = context.getInfo<CL_CONTEXT_DEVICES>();
+        CheckError(!devices.empty() ? CL_SUCCESS : -1, "");
+        std::string log = program.getBuildInfo<CL_PROGRAM_BUILD_LOG>(devices[0], nullptr);
+        std::cerr << "Build Log:\n" << log << std::endl;
+        exit(-1);
+    }
+
+
+    return program;
+}
 
 int main ()
 {
+    //select platform
     cl::vector<cl::Platform> platforms;
     cl::Platform::get(&platforms);
 
@@ -79,6 +68,7 @@ int main ()
         std::cout << "\t- " << platform.getInfo<CL_PLATFORM_NAME>() << std::endl;
     }
 
+    // select device
     cl::vector<cl::Device> devices;
     platforms[0].getDevices(CL_DEVICE_TYPE_GPU, &devices);
 
@@ -93,84 +83,63 @@ int main ()
         std::cout << "\t- " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
     }
 
-//
-//    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateContext.html
-//    const cl_context_properties contextProperties [] =
-//            {
-//                    CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties> (platformIds [0]),
-//                    0, 0
-//            };
-//
-//    cl_int error = CL_SUCCESS;
-//    cl_context context = clCreateContext (contextProperties, deviceIdCount,
-//                                          deviceIds.data (), nullptr, nullptr, &error);
-//    CheckError (error, "Create Context");
-//
-//    std::cout << "Context created" << std::endl;
-//
-//    cl_program program = BuildProgram (LoadKernel ("kernels/saxpy.cl"),
-//                                        context);
-//
-//
-//
-//    cl_kernel kernel = clCreateKernel (program, "SAXPY", &error);
-//    CheckError (error, "Create kernel");
-//
-//    // Prepare some test data
-//    static const size_t testDataSize = 1 << 10;
-//    std::vector<float> a (testDataSize), b (testDataSize);
-//    for (int i = 0; i < testDataSize; ++i) {
-//        a [i] = static_cast<float> (1);
-//        b [i] = static_cast<float> (1);
-//    }
-//
-//    cl_mem aBuffer = clCreateBuffer (context, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
-//                                     sizeof (float) * (testDataSize),
-//                                     a.data (), &error);
-//    CheckError (error, "Create aBuffer");
-//
-//    cl_mem bBuffer = clCreateBuffer (context, CL_MEM_READ_WRITE | CL_MEM_USE_HOST_PTR,
-//                                     sizeof (float) * (testDataSize),
-//                                     b.data (), &error);
-//    CheckError (error, "create bBuffer");
-//
-//    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clCreateCommandQueue.html
-//    cl_command_queue queue = clCreateCommandQueue (context, deviceIds [0],
-//                                                   0, &error);
-//    CheckError (error, "create queue");
-//
-//    clSetKernelArg (kernel, 0, sizeof (cl_mem), &aBuffer);
-//    clSetKernelArg (kernel, 1, sizeof (cl_mem), &bBuffer);
-//    static const float two = 2.0f;
-//    clSetKernelArg (kernel, 2, sizeof (float), &two);
-//
-//    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clEnqueueNDRangeKernel.html
-//    const size_t globalWorkSize [] = { testDataSize, 0, 0 };
-//    CheckError (clEnqueueNDRangeKernel (queue, kernel, 1,
-//                                        nullptr,
-//                                        globalWorkSize,
-//                                        nullptr,
-//                                        0, nullptr, nullptr), "Run kernel");
-//
-//    // Get the results back to the host
-//    // http://www.khronos.org/registry/cl/sdk/1.1/docs/man/xhtml/clEnqueueReadBuffer.html
-//    CheckError (clEnqueueReadBuffer (queue, bBuffer, CL_TRUE, 0,
-//                                     sizeof (float) * testDataSize,
-//                                     b.data (),
-//                                     0, nullptr, nullptr), "read result");
-//
-//    for (float & i : b)
-//        std::cout << i << ' ';
-//
-//    clReleaseCommandQueue (queue);
-//
-//    clReleaseMemObject (bBuffer);
-//    clReleaseMemObject (aBuffer);
-//
-//    clReleaseKernel (kernel);
-//    clReleaseProgram (program);
-//
-//    clReleaseContext (context);
+    // create a context using the platform and devices
+    cl_context_properties cps[3] = {
+            CL_CONTEXT_PLATFORM,
+            (cl_context_properties)(platforms[0])(),
+            0
+    };
+    cl_int error = CL_SUCCESS;
+    cl::Context context(devices, cps, nullptr, nullptr, &error);
+    CheckError (error, "Create Context");
 
-//    https://anteru.net/blog/2012/getting-started-with-opencl-part-3/
+    std::cout << "Context created" << std::endl;
+
+    cl::Program program = BuildProgram (LoadKernel ("kernels/saxpy.cl"),
+                                        context);
+
+    cl::Kernel kernel(program, "SAXPY", &error);
+    CheckError (error, "Create kernel");
+
+    // Prepare some test data
+    static const size_t testDataSize = 1 << 10;
+    std::vector<float> a (testDataSize), b (testDataSize);
+    for (int i = 0; i < testDataSize; ++i) {
+        a [i] = static_cast<float> (1);
+        b [i] = static_cast<float> (1);
+    }
+
+    // Create memory buffers
+    cl::Buffer aBuffer(context,
+            CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+            sizeof (float) * (testDataSize),
+            a.data(), &error);
+    CheckError (error, "Create aBuffer");
+
+    cl::Buffer bBuffer(context,
+                       CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR,
+                       sizeof (float) * (testDataSize),
+                       b.data(), &error);
+    CheckError (error, "Create bBuffer");
+
+    cl::CommandQueue queue(context, devices[0], 0, &error);
+    CheckError (error, "create queue");
+    // Set arguments to kernel
+    kernel.setArg(0, aBuffer);
+    kernel.setArg(1, bBuffer);
+    static const float two = 2.0f;
+    kernel.setArg(2, two);
+
+    cl::NDRange globalWorkSize(testDataSize);
+    cl::NDRange local(cl::NullRange);
+    error = queue.enqueueNDRangeKernel(kernel, cl::NullRange, globalWorkSize, local);
+    CheckError (error, "Run kernel");
+
+    error = queue.enqueueReadBuffer(bBuffer, CL_TRUE, 0, sizeof (float) * testDataSize, b.data ());
+    CheckError(error, "read result");
+
+    for (float & i : b)
+        std::cout << i << ' ';
+
+    //No Release due to RAII
 }
