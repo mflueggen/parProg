fe1bb9880e064ce3a74e4723cb770179dec06025
Benedikt Bock <mail@benedikt1992.de>
2019-07-14 07:00:52 -0700

    working single rank

--- assignment5/mpiavg/main.cpp
+++ assignment5/mpiavg/main.cpp
@@ -1,8 +1,16 @@
 #include <mpi.h>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
 
 int main(int argc, char** argv) {
+    // use fixed 6 digit precision for output
+    std::cout << std::fixed;
+    std::cout << std::setprecision(6);
+    
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
 
@@ -33,15 +41,40 @@ int main(int argc, char** argv) {
     MPI_Comm_size(precision_comm, &precision_size);
     //TODO root communicator with the first ranks of each precision communicator (to efficiently exchange the results)
 
-    if (is_integer_precision) {
-        printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
-           world_rank, world_size, precision_rank, precision_size);
+
+    //Get data
+    //TODO make it failsave (no guarantees)
+    std::ifstream input(data_file);
+    std::vector<double> data;
+    std::string line;
+    while (input >> line)
+        data.push_back(std::stod(line));
+
+
+//        printf("WORLD RANK/SIZE: %d/%d \t ROW RANK/SIZE: %d/%d\n",
+//           world_rank, world_size, precision_rank, precision_size);
+//        std::cout << data.size() << std::endl;
+
+        int chunk = std::ceil(data.size() / (float) precision_size);
+
+        chunk = precision_rank * chunk + chunk > data.size() ? data.size() - precision_rank * chunk : chunk;
+        double sum = 0;
+        for (int i = precision_rank * chunk; i < precision_rank * chunk + chunk; ++i) {
+            if (is_integer_precision)
+                sum += std::floor(data[i]);
+            else
+                sum += data[i];
+
+        }
+        sum /= chunk;
+        if (precision_rank == 0)
+                std::cout << sum << std::endl;
         // TODO calc integer precision average and use reduce to get the results
         // TODO receive double preciison result.
-    } else {
+
         //TODO calc double precision average and use reduce as well
         //TODO send result to integer root via root communicator
-    }
+
 
     MPI_Comm_free(&precision_comm);
     // Finalize the MPI environment.
