a4c4959f0318faf44a13722200521ea97bbce6c2
Benedikt Bock <mail@benedikt1992.de>
2019-07-14 10:21:02 -0700

    add comments

--- assignment5/mpiavg/mpiavg.cpp
+++ assignment5/mpiavg/mpiavg.cpp
@@ -11,40 +11,33 @@ int main(int argc, char** argv) {
     std::cout << std::fixed;
     std::cout << std::setprecision(6);
 
-    // Initialize the MPI environment
-    MPI_Init(NULL, NULL);
-
+    // Parse arguments
     std::string data_file = argv[1];
     int integer_precision_size = std::stoi(argv[2]);
     int double_precision_size = std::stoi(argv[3]);
 
+    // Initialize the MPI environment and all communicators
+    MPI_Init(NULL, NULL);
 
-    // Get the number of processes
     int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-    // Get the rank of the process
     int world_rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
-
-    bool is_integer_precision = world_rank / double_precision_size;
-    int color = is_integer_precision ? 1 : 0; // split ranks into integer and double precision group
-
-// Split the communicator based on the color and use the
-// original rank for ordering
     MPI_Comm precision_comm;
-    MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &precision_comm);
     int precision_rank, precision_size;
+    bool is_integer_precision = world_rank / double_precision_size;
+    int color = is_integer_precision ? 1 : 0; // split ranks into integer and double precision group. we assume that world_size == integer_precision_size + double_precision_size
+    MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &precision_comm);
     MPI_Comm_rank(precision_comm, &precision_rank);
     MPI_Comm_size(precision_comm, &precision_size);
 
-    color = precision_rank == 0 ? 1 : 0;
     MPI_Comm root_comm;
+    color = precision_rank == 0 ? 1 : 0;
     MPI_Comm_split(MPI_COMM_WORLD, color, world_rank, &root_comm);
 
 
-    //Get data
+    //Get data, broadcast data between ranks if necessary
     std::ifstream input(data_file);
     std::vector<double> data;
     std::string line;
@@ -70,14 +63,16 @@ int main(int argc, char** argv) {
     if (distribute_data) {
         if (world_rank != data_source_rank)
             data.resize(size_report[data_source_rank]);
+        
+        // We do no split up the data since
+        // 1. We are not allowed to use p2p mechanisms
+        // 2. the data should be accessible by all ranks anyways if the data size has a significant impact on the broadcast.
         MPI_Bcast(data.data(), size_report[data_source_rank], MPI_DOUBLE, data_source_rank, MPI_COMM_WORLD);
     }
 
 
-
-
+    // Each rank calculates the sum of its input fraction
     int global_chunk = std::ceil(data.size() / (float) precision_size);
-
     int local_chunk = precision_rank * global_chunk + global_chunk > data.size() ? data.size() - precision_rank * global_chunk : global_chunk;
     double sum = 0;
     for (int i = precision_rank * global_chunk; i < precision_rank * global_chunk + local_chunk; ++i) {
@@ -88,15 +83,15 @@ int main(int argc, char** argv) {
     }
 
 
-    // Reduce all of the local averages into the global average
+    // Reduce all of the local sums into the global sum and calculate average
     double global_avg;
     MPI_Reduce(&sum, &global_avg, 1, MPI_DOUBLE, MPI_SUM, 0,
                precision_comm);
     global_avg /= data.size();
 
 
+    // Gather both averages on the global root process
     if (precision_rank == 0) {
-        // Gather both averages on the global root process
         double averages[2]; //we only have two rank in the root communicator
         MPI_Gather(&global_avg, 1, MPI_DOUBLE, &averages, 1,
                    MPI_DOUBLE, 0, root_comm);
@@ -108,7 +103,7 @@ int main(int argc, char** argv) {
     }
 
 
+    MPI_Comm_free(&root_comm);
     MPI_Comm_free(&precision_comm);
-    // Finalize the MPI environment.
     MPI_Finalize();
 }
